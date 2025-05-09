c3aa666f9e51dbc6a18e44649ad6d1a41430ba64
Varun Santhanam <talkto@vsanthanam.com>
2025-05-09 11:55:51 -0400

    Separate benchmarks into separate package, add Windows workflow (#6)

--- Package.swift
+++ Package.swift
@@ -23,10 +23,6 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/ordo-one/package-benchmark",
-            from: "1.0.0"
-        ),
-        .package(
             url: "https://github.com/nicklockwood/SwiftFormat.git",
             exact: "0.55.6"
         )
@@ -46,37 +42,6 @@ let package = Package(
             name: "JaybirdTests",
             dependencies: [
                 "Jaybird"
-            ]
-        ),
-        .executableTarget(
-            name: "JaybirdBenchmark",
-            dependencies: [
-                .product(name: "Benchmark", package: "package-benchmark"),
-                "Jaybird"
-            ],
-            path: "Benchmarks/JaybirdBenchmark",
-            resources: [
-                .copy("benchmark-micro.json"),
-                .copy("benchmark-micro-pretty.json"),
-                .copy("benchmark-64kb.json"),
-                .copy("benchmark-64kb-pretty.json"),
-                .copy("benchmark-256kb.json"),
-                .copy("benchmark-256kb-pretty.json"),
-                .copy("benchmark-512kb.json"),
-                .copy("benchmark-512kb-pretty.json"),
-                .copy("benchmark-1mb.json"),
-                .copy("benchmark-1mb-pretty.json"),
-                .copy("benchmark-5mb.json"),
-                .copy("benchmark-5mb-pretty.json"),
-                .copy("benchmark-strings.json"),
-                .copy("benchmark-numeric.json"),
-                .copy("benchmark-integers.json"),
-            ],
-            swiftSettings: [
-                .define("BENCHMARK_DISABLE_JEMALLOC")
-            ],
-            plugins: [
-                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
             ]
         )
     ]
